001a5bc7701d6a4e2cba7898b5cedaa9f9709d39
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-26 15:28:34 -0400

    created a show.erb in the users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
     post '/users/signup' do
         @user = User.create(username: params[:username], password: params[:password])
-    
+        redirect "/users/#{@user.id}"
     end
 
     get '/users/:id' do
-
+        #erb :'/users/show'
+        "show page"
     end
 
 end
