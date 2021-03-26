585ae5945374d30ea5cf47aac43fea40528a92ca
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-26 18:59:18 -0400

    created a post users login method in the users controller that will authenticate a user at the login and then redirect them to their homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,12 @@ class UsersController < ApplicationController
 
     post '/users/logn' do
         @user = User.find_by(username: params[:username])
-
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}"
+        else
+            redirect "/users/login"
+        end
     end
 
     get '/users/:id' do
