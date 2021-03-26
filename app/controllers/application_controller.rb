1bd42c43467a5d730601fec6a1c7cce0967e61aa
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-26 19:51:54 -0400

    incorporated helper method logged_in? to the get'/' method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect "/users/#{session[:user_id]}"
+    else
+      erb :welcome
+    end
   end
 
   helpers do
