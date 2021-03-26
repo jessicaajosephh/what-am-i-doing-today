6580ad80dcae73c6387c97af047bbb9f520e7a67
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-26 19:10:34 -0400

    spelling error in method name changed '/users/logn' to '/users/login'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,14 @@ class UsersController < ApplicationController
         erb :'/users/login'
     end
 
-    post '/users/logn' do
+    post '/users/login' do
         @user = User.find_by(username: params[:username])
+
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect "/users/#{@user.id}"
         else
-            redirect "/users/login"
+            redirect '/users/login'
         end
     end
 
