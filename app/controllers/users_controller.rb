e91eac6ff24ca45cf05a57af4fb49e0cfd32f6a7
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-26 17:19:42 -0400

    created a get and post method for a user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,14 @@ class UsersController < ApplicationController
         redirect "/users/#{@user.id}"
     end
 
+    get '/users/login' do
+
+    end
+
+    post '/users/logn' do
+
+    end
+
     get '/users/:id' do
         @user = User.find(params[:id])
         erb :'/users/show'
